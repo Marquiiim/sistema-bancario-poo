f702ea0ef98aac9a7eaf08786b27a046df75aff2
Marcos Gabriel <marcosindev@gmail.com>
2025-03-18 16:38:45 -0300

    add property in class

--- src/models/usuario.py
+++ src/models/usuario.py
@@ -3,14 +3,27 @@ from datetime import datetime
 class Client:
     def __init__(self, balance=0, withdraw_limit=1000):
         self._balance = balance
+        self._withdraw_limit = withdraw_limit
         self.number_transactions = 0
-        self.withdraw_limit = withdraw_limit
         self.extract = {"Depósitos": [], "Saques": []}
 
-    def deposit_client(self, value):  
+    @property
+    def balance(self):
+        return self._balance
+    
+    @balance.setter
+    def balance(self, value):
         if not isinstance(value, (int, float)):
-            print("[ERROR] O valor deve conter apenas números.")
+            raise ValueError("[ERROR] O valor deve conter apenas números.")
+        if value < 0:
+            raise ValueError("[ERROR] Valor inválido, tente novamente")
+        self._balance = value
 
+    @property
+    def withdraw_limit(self):
+        return self._withdraw_limit
+        
+    def deposit_client(self, value):
         date_now = datetime.now()
         date_str = date_now.strftime('%d/%m/%Y')
         time_str = date_now.strftime('%H:%M:%S')
@@ -18,20 +31,15 @@ class Client:
         if self.number_transactions >= 10:
             print("[ERROR] Você atingiu o número de transações diárias, tente novamente outro dia.")
             return False
-        elif value <= 0:
-            print("[ERROR] Valor inválido, tente novamente")
-            return False
         else:
-            self._balance += value
+            self.balance += value
             self.number_transactions += 1
             self.extract["Depósitos"].append(f"R${value:.2f} | Dia: {date_str} às {time_str}")
             print(f"Depósito de R${value:.2f} realizado com sucesso.")
             return True
+        
     
     def withdraw_client(self, value):
-        if not isinstance(value, (int, float)):
-            print("[ERROR] O valor deve conter apenas números.")
-        
         date_now = datetime.now()
         date_str = date_now.strftime('%d/%m/%Y')
         time_str = date_now.strftime('%H:%M:%S')
@@ -40,29 +48,24 @@ class Client:
             print("[ERROR] Você atingiu o número de transações diárias, tente novamente outro dia.")
             return False
         elif value > self.withdraw_limit:
-            print("[ERROR] Você não pode sacar mais de R$1000,00")
+            print(f"[ERROR] Você não pode sacar mais de R${self.withdraw_limit:.2f}.")
             return False
-        elif value > self._balance:
+        elif value > self.balance:
             print("[ERROR] Saldo insuficiente para o saque, tente novamente.")
             return False
         else:
-            self._balance -= value
+            self.balance -= value
             self.number_transactions += 1
             self.extract["Saques"].append(f"R${value:.2f} | Dia: {date_str} às {time_str}")
             print(f"Saque de R${value:.2f} realizado com sucesso.")
             return True
 
     def view_balance(self):
-        return f"{self._balance:.2f}"
+        return f"{self.balance:.2f}"
 
     def extract_client(self):
-        line_break = '\n    '
-        print(f"""
-========= EXTRATO DE DEPÓSITOS =========
-{line_break.join(self.extract["Depósitos"])}
-""")
+        print("========= EXTRATO DE DEPÓSITOS =========")
+        print("\n".join(self.extract["Depósitos"]) or "Nenhum depósito realizado.")
         
-        print(f"""
-========= EXTRATO DE SAQUES =========
-{line_break.join(self.extract["Saques"])}
-""")
+        print("========= EXTRATO DE SAQUES =========")
+        print("\n".join(self.extract["Saques"]) or "Nenhum saque realizado.")
